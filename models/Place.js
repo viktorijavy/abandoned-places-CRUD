375489d097232e8274c1805f5ccfe03eba89e40f
Viktorija Kuneviciute <viktorija.qun@gmail.com>
2022-01-17 21:32:59 +0100

    added maps and home page

--- models/Place.js
+++ models/Place.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//to include virtuals in res.json() we need to set toJSON schema option to {virtuals: true} and we pass that into our schema
+const opts = { toJSON: { virtuals: true } };
+
 const placeSchema = new Schema ({
     title: String,
     description: String,
@@ -31,9 +34,14 @@ const placeSchema = new Schema ({
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
-    ],
-    
-});
+    ]
+}, opts);
+
+placeSchema.virtual('properties.popUpMarkup').get(function() {
+    return `
+    <a href="/places/${this._id}"> ${this.title}</a>
+    <p> ${this.description.substring(0, 30)} </p>`
+})
 
 const Place = mongoose.model('Place', placeSchema)
 module.exports = Place
